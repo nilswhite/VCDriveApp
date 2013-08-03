8a92e922e49cde42ce98a2da3232884da1dcca8d
Chris White <christopher.nils.white@gmail.com>
2013-08-03 00:02:54 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VCDriveApp::Application.config.secret_token = '45dd7177449652d94b153c9fbc18676d8d0ad5b7ccd450507396e3c97b8e59ee9db10827fdcf91a06121978c83ec8203df05d5419c394bd161931bfe4ac4e2da'
